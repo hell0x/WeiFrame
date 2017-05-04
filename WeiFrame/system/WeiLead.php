bb3272c450afd9a3540d504d6e58a5fcbc8ae61c
starryskywx <327466897@qq.com>
2017-05-04 19:16:30 +0800

    frame

--- WeiFrame/system/WeiLead.php
+++ WeiFrame/system/WeiLead.php
@@ -6,6 +6,7 @@ require_once BASEPATH.'Common.php';
 
 set_error_handler('_error_handler');
 set_exception_handler('_exception_handler');
+// register_shutdown_function('_shutdown_handler');
 
 //错误异常处理
 //暂时不写...
